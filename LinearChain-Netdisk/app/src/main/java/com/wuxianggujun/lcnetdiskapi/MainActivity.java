0a670ef6a6ffece8449b53cfe820aad278d37c2a
WuXiangGuJun <3344207732@qq.com>
2021-05-26 14:35:42 +0800

    新建直链网盘项目

---
+++ LinearChain-Netdisk/app/src/main/java/com/wuxianggujun/lcnetdiskapi/MainActivity.java
@@ -0,0 +1,15 @@
+package com.wuxianggujun.lcnetdiskapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Context;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
